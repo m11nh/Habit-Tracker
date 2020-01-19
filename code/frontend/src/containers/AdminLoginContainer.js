5cd112211940a6a5aa413f04afebd8f49a8b8bb9
duy minh nguyen <greatwhitepup@hotmail.com>
2020-01-19 13:26:01 +1100

    successfully debugged the checkoff bug (will check again tomorrow), completed delete account, now working on admin stuff

---
+++ code/frontend/src/containers/AdminLoginContainer.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+
+function AdminLoginContainer() {
+	const [username, setUsername] = useState("")
+	const [password, setPassword] = useState("")
+
+}
+
+export default AdminLoginContainer
